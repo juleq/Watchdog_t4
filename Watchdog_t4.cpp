9025aa039a4d4f8930c8fa0e9f6962da4b75d4e4
Antonio Brewer <yodabox64@hotmail.com>
2019-01-13 12:57:18 -0500

    Update Watchdog_t4.cpp

--- Watchdog_t4.cpp
+++ Watchdog_t4.cpp
@@ -19,17 +19,23 @@ void watchdog_class::begin() {
   _VectorsRam[16 + IRQ_WDOG1] = watchdog_isr;
   WDOG1_WCR |= WDOG_WCR_WDE | WDOG_WCR_WDT | WDOG_WCR_SRE;
   WDOG1_WMCR = 0;
+  WDOG1_WICR |= (3UL << 14);
   feed();
+}
+
+void watchdog_class::reset() {
+  WDOG1_WCR &= ~(1UL << 4);
 }
 
 void watchdog_class::setTrigger(float val) {
   if ( val < 0 ) val = 0;
   else if ( val > 127.5 ) val = 127.5;
-  WDOG1_WICR |= (3UL << 14) | (uint16_t)(val / 0.5f);
+  if ( val >= ((((WDOG1_WCR & 0xFF00 ) >> 8 ) + 1) *0.5f) ) val = 2;
+  WDOG1_WICR |= (WDOG1_WICR & ~0xFF) | (3UL << 14) | (uint16_t)(val / 0.5f);
 }
 
 void watchdog_class::setTimeout(float val) {
-  if ( val < 0.5 ) val = 0.5;
+  if ( val < 0.5f ) val = 0.5f;
   else if ( val > 128 ) val = 128;
   uint8_t value = (float)((  val / 0.5f) - 1);
   WDOG1_WCR |= (WDOG1_WCR & ~WDOG_WCR_WT) | value << 8;
